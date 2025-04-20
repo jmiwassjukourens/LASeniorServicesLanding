818f2d2a2d9639b78886d4f0eb3cb6b925fe869c
jmiwassjukourens <jiwassjuk@gmail.com>
2025-04-20 15:46:33 -0300

    about us - section 1 and 2

--- src/main.server.ts
+++
@@ -1,7 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/components/app-component/app.component';
-import { config } from './app/components/app-component/app.config.server';
-
-const bootstrap = () => bootstrapApplication(AppComponent, config);
-
-export default bootstrap;
